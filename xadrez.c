56188c3e8836119e03294b79a75ca51b688a06d2
dani-dantas <132109303+dani-dantas@users.noreply.github.com>
2025-02-18 13:17:56 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -7,29 +7,30 @@
 
 #define TAMANHO_TABULEIRO 8
 
-wchar_t tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {
-    {L'♖', L'♘', L'♗', L'♕', L'♔', L'♗', L'♘', L'♖'},
-    {L'♙', L'♙', L'♙', L'♙', L'♙', L'♙', L'♙', L'♙'},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L'♟', L'♟', L'♟', L'♟', L'♟', L'♟', L'♟', L'♟'},
-    {L'♜', L'♞', L'♝', L'♛', L'♚', L'♝', L'♞', L'♜'}
+// Alterar para representar peças com letras
+char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {
+    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
 };
 
-int cursor_x = 0, cursor_y = 0;
+int posicao_x = 0, posicao_y = 0;
 bool selecionando = false;
-int selecionado_x = -1, selecionado_y = -1;
+int posicao_selecionada_x = -1, posicao_selecionada_y = -1;
 
 void exibir_tutorial() {
     clear();
     mvprintw(5, 10, "Bem-vindo ao Jogo de Xadrez!");
     mvprintw(7, 10, "Use as setas do teclado para mover o cursor.");
-    mvprintw(8, 10, "Pressione ENTER para selecionar uma peca.");
-    mvprintw(9, 10, "Pressione ENTER novamente para mover a peca.");
+    mvprintw(8, 10, "Pressione ENTER para selecionar uma peça.");
+    mvprintw(9, 10, "Pressione ENTER novamente para mover a peça.");
     mvprintw(10, 10, "Pressione 'q' para sair do jogo.");
-    mvprintw(12, 10, "Voce esta pronto? Aperte qualquer tecla para comecar.");
+    mvprintw(12, 10, "Você está pronto? Aperte qualquer tecla para começar.");
     refresh();
     getch();
 }
@@ -39,18 +40,22 @@ void desenhar_tabuleiro(WINDOW *janela) {
     box(janela, 0, 0);
     for (int y = 0; y < TAMANHO_TABULEIRO; y++) {
         for (int x = 0; x < TAMANHO_TABULEIRO; x++) {
+            // Casas alternadas
             if ((y + x) % 2 == 0) {
-                wattron(janela, COLOR_PAIR(1));
+                wattron(janela, COLOR_PAIR(1)); // Casa branca
             } else {
-                wattron(janela, COLOR_PAIR(2));
+                wattron(janela, COLOR_PAIR(2)); // Casa preta
             }
-            if (y == cursor_y && x == cursor_x) {
-                wattron(janela, A_REVERSE);
+
+            if (y == posicao_y && x == posicao_x) {
+                wattron(janela, A_REVERSE); // Destacar o cursor
             }
-            if (selecionando && y == selecionado_y && x == selecionado_x) {
-                wattron(janela, A_BOLD);
+
+            if (selecionando && y == posicao_selecionada_y && x == posicao_selecionada_x) {
+                wattron(janela, A_BOLD); // Destacar a peça selecionada
             }
-            mvwprintw(janela, y + 1, x * 4 + 2, "%lc", tabuleiro[y][x]);
+
+            mvwprintw(janela, y + 1, x * 4 + 2, "%c", tabuleiro[y][x]);
             wattroff(janela, A_REVERSE);
             wattroff(janela, A_BOLD);
             wattroff(janela, COLOR_PAIR(1));
@@ -61,20 +66,20 @@ void desenhar_tabuleiro(WINDOW *janela) {
 }
 
 void mover_cursor(int dy, int dx) {
-    cursor_y = (cursor_y + dy + TAMANHO_TABULEIRO) % TAMANHO_TABULEIRO;
-    cursor_x = (cursor_x + dx + TAMANHO_TABULEIRO) % TAMANHO_TABULEIRO;
+    posicao_y = (posicao_y + dy + TAMANHO_TABULEIRO) % TAMANHO_TABULEIRO;
+    posicao_x = (posicao_x + dx + TAMANHO_TABULEIRO) % TAMANHO_TABULEIRO;
 }
 
 void selecionar_ou_mover() {
     if (!selecionando) {
-        if (tabuleiro[cursor_y][cursor_x] != L' ') {
+        if (tabuleiro[posicao_y][posicao_x] != ' ') {
             selecionando = true;
-            selecionado_x = cursor_x;
-            selecionado_y = cursor_y;
+            posicao_selecionada_x = posicao_x;
+            posicao_selecionada_y = posicao_y;
         }
     } else {
-        tabuleiro[cursor_y][cursor_x] = tabuleiro[selecionado_y][selecionado_x];
-        tabuleiro[selecionado_y][selecionado_x] = L' ';
+        tabuleiro[posicao_y][posicao_x] = tabuleiro[posicao_selecionada_y][posicao_selecionada_x];
+        tabuleiro[posicao_selecionada_y][posicao_selecionada_x] = ' ';
         selecionando = false;
     }
 }
@@ -86,8 +91,8 @@ void inicializar_ncurses() {
     curs_set(0);
     keypad(stdscr, TRUE);
     start_color();
-    init_pair(1, COLOR_WHITE, COLOR_BLUE);
-    init_pair(2, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(1, COLOR_WHITE, COLOR_BLUE);  // Casa branca
+    init_pair(2, COLOR_BLACK, COLOR_YELLOW); // Casa preta
 }
 
 void jogar() {
